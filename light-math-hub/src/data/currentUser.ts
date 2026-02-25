9d0f72b6311b01b3e7e436c9fe50a0acb41ea21f
lee <lifengmeister@gmail.com>
2026-02-25 18:29:29 +0800

    cube

--- light-math-hub/src/data/currentUser.ts
+++ light-math-hub/src/data/currentUser.ts
@@ -1,5 +1,5 @@
 export const currentUser = {
   name: "Math Teacher",
   role: "教学管理员",
-  permissions: ["light.app.fraction.lab"]
+  permissions: ["light.app.fraction.lab", "light.app.cube.surface.lab"]
 };
